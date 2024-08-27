39a6317b4af5bfdef47a274127bea530c80f28d6
diegofgarciao <diegofgarciao@gmail.com>
2024-08-27 00:37:26 -0500

    Deploy build to gh-pages

--- src/app/dashboard/services/transactionsService.js
+++
@@ -1,9 +0,0 @@
-const transactionsService = {
-    getTransactions: async () => {
-        const response = await fetch('https://bold-fe-api.vercel.app/api');
-        const data = await response.json();
-        return data.data || [];
-    }
-};
-
-export default transactionsService;
